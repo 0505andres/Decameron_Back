377e53a1e02c51ee0b7ab92d97dd0d96d606f2a9
0505andres <0505andres@gmail.com>
2026-07-03 02:30:53 -0500

    update_respository

---
+++ src/Repository/ReservaRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Reserva;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+final class ReservaRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Reserva::class);
+    }
+
+    /**
+     * @return array<int, array<string, mixed>>
+     */
+    public function findForList(): array
+    {
+        $qb = $this->createQueryBuilder('r')
+            ->select([
+                'r.id AS id',
+                'r.cliente AS cliente',
+                'r.numeroDocumento AS numeroDocumento',
+                'r.edad AS edad',
+                'r.fechaReserva AS fechaReserva',
+                'r.fechaIngreso AS fechaIngreso',
+                'r.fechaSalida AS fechaSalida',
+                'hotel.id AS hotelId',
+                'hotel.nombre AS hotelNombre',
+                'habitacion.id AS habitacionId',
+                'habitacion.codigo AS habitacionCodigo',
+            ])
+            ->leftJoin('r.hotel', 'hotel')
+            ->leftJoin('r.habitacion', 'habitacion')
+            ->orderBy('r.id', 'ASC');
+
+        return $qb->getQuery()->getArrayResult();
+    }
+}
